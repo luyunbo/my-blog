a68d87c9bcfefeb4a7719424747826a996ddfd17
luyunbo <luyunbo47@gmail.com>
2017-12-04 22:41:47 +0800

    support file name parameter

--- new_blog.sh
+++ new_blog.sh
@@ -8,8 +8,15 @@ fi
 
 __DIR__=`cd "$(dirname "${__FILE__}")"; pwd`
 
+blog_name='untitled.md'
+if [ "$1" == "-n" ];then
+    blog_name="$2".md;
+fi
+    
 
-new_blog=${__DIR__}/_blog/untitled.md
-touch ${new_blog}
+new_blog=${__DIR__}/_blog/${blog_name}
+if [ ! -f ${new_blog} ]; then
+    touch ${new_blog}
+fi
 echo "has new blog: ${new_blog}"
 open ${new_blog}
